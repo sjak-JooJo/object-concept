11ab7fd383987d9a7fb8b0bc4caea6a64327019d
JooJo <sjak0101@gmail.com>
2022-06-21 23:51:22 +0100

    added some new object

--- bind-method.js
+++ bind-method.js
@@ -1,9 +1,27 @@
-const Kibria = {
+const Kibria  = {
     id: 101,
     money: 5000,
     name: 'RJ Kibrea', 
-    treatDey: function(expense, boksis){
-        this.money = this.money - expense - boksis;
+    treatDey: function(expense){
+        this.money = this.money - expense;
+        console.log('herer6767', this);
         return this.money;
     }
 }
+const heroBalam = {
+    id:102,
+    money: 6000,
+    name: 'Hero Balam'
+}
+const normalGolam = {
+    id:102,
+    money: 8000,
+    name: 'Hero Balam'
+}
+
+Kibria.treatDey(100);
+
+const heroTrealDey = Kibria.treatDey.bind(heroBalam);
+heroTrealDey(500);
+const normalTrealDey = Kibria.treatDey.bind(normalGolam);
+normalTrealDey(2000);
